424b22d95ceb579ec615ead347c21c6a49b9d2a7
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-04-26 17:46:53 +0900

    Fix losing state when reopening ToolWindow

---
+++ src/main/kotlin/greeting/GreetingCounterStateService.kt
@@ -0,0 +1,11 @@
+package greeting
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import com.intellij.openapi.components.Service
+
+@Service(Service.Level.PROJECT)
+class GreetingCounterStateService {
+    var counter by mutableIntStateOf(0)
+}
--- src/main/kotlin/greeting/GreetingToolWindowFactory.kt
+++ src/main/kotlin/greeting/GreetingToolWindowFactory.kt
@@ -1,9 +1,6 @@
 package greeting
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
@@ -13,10 +10,10 @@ import org.jetbrains.jewel.ui.component.Text
 
 class GreetingToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
+        val counterStateService = project.service<GreetingCounterStateService>()
         toolWindow.addComposeTab {
-            var counter by remember { mutableIntStateOf(0) }
-            DefaultButton(onClick = { counter++ }) {
-                Text("Click me!! $counter")
+            DefaultButton(onClick = { counterStateService.counter++ }) {
+                Text("Click me!! ${counterStateService.counter}")
             }
         }
     }
