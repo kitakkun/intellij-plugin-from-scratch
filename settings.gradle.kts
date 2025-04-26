0d413cdd21b85d55ce88aef2234d1bb1a310cc75
kitakkun <48154936+kitakkun@users.noreply.github.com>
2025-04-26 17:07:38 +0900

    Add settings.gradle.kts and configure Gradle project

---
+++ settings.gradle.kts
@@ -0,0 +1,14 @@
+rootProject.name = "intellij-plugin-from-scratch"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
